5a3309d04531f116edf83ee58b14fbd8c3b97bbb
rezatn0934 <rezatn0934@gmail.com>
2023-09-13 13:00:11 +0330

    chore: define XmlLink

--- rssfeeds/models.py
+++ rssfeeds/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from core.models import Type, Category
+
+
+class XmlLink(models.Model):
+    xml_link = models.URLField(unique=True)
+    rss_type = models.ForeignKey(Type, on_delete=models.PROTECT)
+
