3d55948ed713311b1548855b63575e61d03ada7e
rezatn0934 <rezatn0934@gmail.com>
2023-09-13 13:05:22 +0330

    chore: define Type models

--- core/models.py
+++ core/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
     parent = models.ForeignKey('self', on_delete=models.CASCADE)
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=50)
