ceb1d23eea6b455816feb4a5be611509ce5d179d
rezatn0934 <rezatn0934@gmail.com>
2023-09-13 13:11:19 +0330

    chore: define Subscription models

--- interactions/models.py
+++ interactions/models.py
@@ -19,3 +19,10 @@ class Comment(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='subscriptions')
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE, related_name='subscriptions')
+    created_at = models.DateTimeField(auto_now_add=True)
