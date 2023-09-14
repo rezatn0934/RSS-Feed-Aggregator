0b56ae29a010c02947ef10d9a999a1e3c954f34d
rezatn0934 <rezatn0934@gmail.com>
2023-09-14 18:36:51 +0330

    chore:define generate_refresh_token in accounts utils

--- accounts/utils.py
+++ accounts/utils.py
@@ -16,3 +16,16 @@ def generate_access_token(user_id, jti):
         access_token_payload, settings.SECRET_KEY, algorithm='HS256')
     return access_token
 
+
+def generate_refresh_token(user_id, jti, ttl):
+    refresh_token_payload = {
+        "token_type": "refresh",
+        'user_id': user_id,
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=ttl),
+        'iat': datetime.datetime.utcnow(),
+        'jti': jti,
+
+    }
+    refresh_token = jwt.encode(
+        refresh_token_payload, settings.SECRET_KEY, algorithm='HS256')
+    return refresh_token
