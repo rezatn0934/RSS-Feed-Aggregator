95718ed1392b16a89881746afa4e0e6a677b0aa5
rezatn0934 <rezatn0934@gmail.com>
2023-09-14 19:11:08 +0330

    chore: define item_model_mapper in rssfeeds utils

--- rssfeeds/utils.py
+++ rssfeeds/utils.py
@@ -8,3 +8,12 @@ def parse_podcast_data(xml_link):
     response = requests.get(xml_link.xml_link)
     [Parser, model] = item_model_mapper(xml_link.rss_type.name)
     return [Parser(response.text).parse_xml_and_create_records(), model]
+
+
+def item_model_mapper(arg):
+    choice = {
+        "Podcast": [PodcastParser, Podcast],
+        "News": [NewsParser, News],
+    }
+    return choice[arg.capitalize()]
+
