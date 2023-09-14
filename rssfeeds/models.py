24131bc8d5b9dd89bf82bc8a23f6ec69ac4bc420
rezatn0934 <rezatn0934@gmail.com>
2023-09-14 22:54:34 +0330

    chore: define dunder str for AbstractRssItem model

--- rssfeeds/models.py
+++ rssfeeds/models.py
@@ -35,6 +35,9 @@ class AbstractRssItem(models.Model):
     class Meta:
         abstract = True
 
+    def __str__(self):
+        return self.title
+
 
 class Podcast(AbstractRssItem):
     duration = models.CharField(null=True, blank=True)
