0bf1fd63d61348dabca64f877dfb69603c2ba583
rezatn0934 <rezatn0934@gmail.com>
2023-09-14 18:32:47 +0330

    chore:define UserRegisterSerializer

---
+++ accounts/serilizers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import User
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(style={"input_type": "password"}, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password2']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+
