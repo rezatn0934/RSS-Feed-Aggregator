3920a5215b79b3769f4583c60115deac06062b9b
rezatn0934 <rezatn0934@gmail.com>
2023-09-14 18:44:58 +0330

    chore:define accounts urls

--- config/urls.py
+++ config/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('', include('accounts.urls')),
 ]
