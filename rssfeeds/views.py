16b1d74ab6e1030c30b670178cf6b12ad8b01a9e
rezatn0934 <rezatn0934@gmail.com>
2023-09-14 23:12:59 +0330

    chore: define create in XmlLink ViewSet

--- rssfeeds/views.py
+++ rssfeeds/views.py
@@ -14,3 +14,29 @@ from .utils import parse_podcast_data, create_or_update_categories
 class XmlLinkViewSet(CreateModelMixin, DestroyModelMixin, ListModelMixin, RetrieveModelMixin, GenericViewSet):
     serializer_class = XmlLinkSerializer
     queryset = XmlLink.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        xml_link = serializer.save()
+
+        [data, model] = parse_podcast_data(xml_link)
+
+        categories = create_or_update_categories(data.get('channel_data')['categories'])
+
+        channel_data = data.get('channel_data')['data']
+        channel, created = Channel.objects.get_or_create(xml_link=xml_link, defaults=channel_data)
+        if not created:
+            for key, value in channel_data.items():
+                setattr(channel, key, value)
+            channel.save()
+
+        channel.category.set(categories)
+
+        podcast_data = data.get('podcast_data')
+        podcast_items = [model(channel=channel, **item) for item in podcast_data]
+        model.objects.bulk_create(podcast_items)
+
+        return Response('ok', status=status.HTTP_201_CREATED)
+
