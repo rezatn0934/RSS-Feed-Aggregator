f5b26a059bb6c4e465c2f5b05f03d07870176b89
rezatn0934 <rezatn0934@gmail.com>
2023-09-14 23:16:42 +0330

    chore: define router for NewsViewSet

--- rssfeeds/urls.py
+++ rssfeeds/urls.py
@@ -6,6 +6,7 @@ router = DefaultRouter()
 router.register('rssfeeds', views.XmlLinkViewSet)
 router.register('channels', views.ChannelViewSet)
 router.register('podcasts', views.PodcastViewSet)
+router.register('news', views.NewsViewSet)
 
 app_name = 'rssfeeds'
 urlpatterns = router.urls
