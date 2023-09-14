83ccdd3aec2ade0a2c9279ab155a42548ce7dfa6
rezatn0934 <rezatn0934@gmail.com>
2023-09-14 18:35:24 +0330

    chore:define refresh token urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ app_name = 'accounts'
 urlpatterns = [
     path("register/", views.UserRegister.as_view(), name="register"),
     path("login/", views.UserLogin.as_view(), name="login"),
+    path("refresh/", views.RefreshToken.as_view(), name="refresh_token"),
 ]
