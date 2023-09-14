8ec10ef5979558bea0147d28b21b69baf19c15e6
rezatn0934 <rezatn0934@gmail.com>
2023-09-14 18:35:05 +0330

    chore:define login urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path("register/", views.UserRegister.as_view(), name="register"),
+    path("login/", views.UserLogin.as_view(), name="login"),
 ]
