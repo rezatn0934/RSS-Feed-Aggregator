a08588400d4d7f6ec8c4c86dc1fdc884129c40ac
rezatn0934 <rezatn0934@gmail.com>
2023-09-14 23:07:01 +0330

    refactor: refactor XmlLinkSerializer to include id

--- rssfeeds/serializers.py
+++ rssfeeds/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 
-from .models import XmlLink
+from .models import XmlLink, Channel, Podcast, News
 
 
 class XmlLinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = XmlLink
-        fields = ['xml_link', 'rss_type']
+        fields = ['id', 'xml_link', 'rss_type']
+        extra_kwargs = {
+            'id': {'read_only': True}
+        }
+
+
