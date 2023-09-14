d6565e167a562ece0e59e60f9ed938c1e4a58982
rezatn0934 <rezatn0934@gmail.com>
2023-09-14 18:44:25 +0330

    chore:define schema / swagger urls

--- config/urls.py
+++ config/urls.py
@@ -22,4 +22,5 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
