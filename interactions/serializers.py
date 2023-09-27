f95cbd928a2b34c70840e4bcbe554725d9e4b5c3
rezatn0934 <rezatn0934@gmail.com>
2023-09-27 17:54:06 +0330

    chore: define create in SubscriptionSerializer

--- interactions/serializers.py
+++ interactions/serializers.py
@@ -7,3 +7,8 @@ class SubscriptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subscription
         fields = ['channel']
+
+    def create(self, validated_data):
+        channel = validated_data['channel']
+        user = self.context['request'].user
+        return Subscription.objects.create(user=user, channel=channel)
