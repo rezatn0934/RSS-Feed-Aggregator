7db47d7bb3420ba31e7c1ee41ffcda5a0c0a3f08
rezatn0934 <rezatn0934@gmail.com>
2023-09-27 15:15:06 +0330

    chore: define update_recommendations

--- interactions/utils.py
+++ interactions/utils.py
@@ -4,3 +4,10 @@ from .models import Recommendation
 
 def get_item_model(channel):
     return Podcast if hasattr(channel, 'podcast_set') else News
+
+
+def update_recommendations(user, categories, increment_count=1):
+    for category in categories:
+        recommend_obj, created = Recommendation.objects.get_or_create(user=user, category=category)
+        recommend_obj.count += increment_count
+        recommend_obj.save()
