9e1fbb6328000f240be4639d8e23642d9837dbcf
rezatn0934 <rezatn0934@gmail.com>
2023-09-27 10:19:40 +0330

    chore: define Email configs

--- config/settings.py
+++ config/settings.py
@@ -219,5 +219,12 @@ LOGGING = {
     },
 }
 
+EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND')
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 AUTH_USER_MODEL = 'accounts.User'
