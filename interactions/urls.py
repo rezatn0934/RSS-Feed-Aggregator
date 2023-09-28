91969874d11bcead09b7d1d3f020e606ccfd1f1b
rezatn0934 <rezatn0934@gmail.com>
2023-09-29 00:00:32 +0330

    chore: define RecommendationRetrieveView path

--- interactions/urls.py
+++ interactions/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LikeView, CommentView, BookMarkView, SubscriptionView
+from .views import LikeView, CommentView, BookMarkView, SubscriptionView, RecommendationRetrieveView
 
 
 app_name = 'interaction'
@@ -8,4 +8,5 @@ urlpatterns = [
     path('comment/', CommentView.as_view(), name='comment'),
     path('bookmark/', BookMarkView.as_view(), name='bookmark'),
     path('subscription/', SubscriptionView.as_view(), name='subscription'),
+    path('recommendation/', RecommendationRetrieveView.as_view(), name='recommendation'),
 ]
