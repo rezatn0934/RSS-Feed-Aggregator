23a20ea014523d282767c8764407c2886eca6d37
rezatn0934 <rezatn0934@gmail.com>
2023-09-28 18:39:18 +0330

    chore: define get_comments in BaseItemSerializer

--- rssfeeds/serializers.py
+++ rssfeeds/serializers.py
@@ -1,8 +1,8 @@
-
 from django.contrib.contenttypes.models import ContentType
 from rest_framework import serializers
 
-from interactions.models import Like, BookMark
+from interactions.models import Like, BookMark, Comment, Subscription
+from interactions.serializers import CommentSerializer
 from .models import XmlLink, Channel, Podcast, News
 
 
@@ -33,12 +33,14 @@ class ChannelSerializer(serializers.ModelSerializer):
             user=user
         ).exists()
 
+
 class BaseItemSerializer(serializers.ModelSerializer):
     liked = serializers.SerializerMethodField()
     bookmarked = serializers.SerializerMethodField()
+    comments = serializers.SerializerMethodField()
 
     class Meta:
-        fields = ['id', 'title', 'channel', 'liked', 'bookmarked', 'guid', 'pub_date', 'image']
+        fields = ['id', 'title', 'channel', 'comments', 'liked', 'bookmarked', 'guid', 'pub_date', 'image']
 
     def get_liked(self, obj):
         user = self.context['request'].user
@@ -54,6 +56,12 @@ class BaseItemSerializer(serializers.ModelSerializer):
         content_type = ContentType.objects.get_for_model(obj)
         return BookMark.objects.filter(
             object_id=obj.pk, content_type=content_type, user=user).exists()
+
+    def get_comments(self, obj):
+        content_type = ContentType.objects.get_for_model(obj)
+        comments = Comment.objects.filter(object_id=obj.pk, content_type=content_type)
+        comment_serializer = CommentSerializer(comments, many=True)
+        return comment_serializer.data
 
 
 class PodcastSerializer(BaseItemSerializer):
