a0e64318af969517033a5f4b37d49fc0e070de9d
rezatn0934 <rezatn0934@gmail.com>
2023-09-28 18:26:19 +0330

    chore: define CommentSerializer

--- interactions/serializers.py
+++ interactions/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Subscription
+from .models import Subscription, Comment
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
@@ -12,3 +12,12 @@ class SubscriptionSerializer(serializers.ModelSerializer):
         channel = validated_data['channel']
         user = self.context['request'].user
         return Subscription.objects.create(user=user, channel=channel)
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'user', 'content']
+        extra_kwargs = {
+            'id': {'read_only': True}
+        }
