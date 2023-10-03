686f6ccaf373c77b88f311753e9a956bb20d1b08
rezatn0934 <rezatn0934@gmail.com>
2023-10-03 11:08:41 +0330

    refactor: refactor UserProfileDetailView path

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,17 +3,18 @@ from rest_framework.routers import DefaultRouter
 
 from . import views
 
-router = DefaultRouter()
-router.register('users', views.UserProfileDetailView, basename='users')
 
 app_name = 'accounts'
 urlpatterns = [
     path("register/", views.UserRegister.as_view(), name="register"),
     path("login/", views.UserLogin.as_view(), name="login"),
+    path('user/', views.UserProfileDetailView.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}),
+         name='user-detail'),
+    path('user/change_password/', views.UserProfileDetailView.as_view({'post': 'change_password'}),
+         name='user-change-password'),
     path("refresh/", views.RefreshToken.as_view(), name="refresh_token"),
     path("logout/", views.LogoutView.as_view(), name="logout"),
     path("forget_password/", views.ForgetPassword.as_view(), name="forget_password"),
     path("change_password_token/", views.ChangePasswordWithToken.as_view(), name="change_password_token"),
 ]
 
-urlpatterns += router.urls
