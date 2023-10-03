c2e20da18a1d73aef0184725783599204f759232
rezatn0934 <rezatn0934@gmail.com>
2023-10-03 11:10:10 +0330

    refactor: refactor parse data function name

--- rssfeeds/utils.py
+++ rssfeeds/utils.py
@@ -4,7 +4,7 @@ from .models import Podcast, News, Channel
 import requests
 
 
-def parse_podcast_data(xml_link):
+def parse_data(xml_link):
     response = requests.get(xml_link.xml_link)
     [Parser, model] = item_model_mapper(xml_link.rss_type.name)
     return [Parser(response.text).parse_xml_and_create_records(), model]
