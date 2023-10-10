6c957dd7379a58a7ebf38e5978201d384ebd42eb
rezatn0934 <rezatn0934@gmail.com>
2023-10-10 14:08:01 +0330

    refactor: refactor AuthBackend so only active user authenticated

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -20,7 +20,7 @@ class AuthBackend(ModelBackend):
             except user_model.DoesNotExist:
                 return None
 
-        return user if user.check_password(password) else None
+        return user if (user.check_password(password) and user.is_active) else None
 
     def get_user(self, user_id):
         user_model = get_user_model()
