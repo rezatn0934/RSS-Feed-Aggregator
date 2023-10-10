5eff7f515f384d1ea4e1ecde40b056422118dd33
rezatn0934 <rezatn0934@gmail.com>
2023-10-10 14:23:42 +0330

    chore: define EventPublisher

---
+++ accounts/publisher.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+import json
+import os
+import pika
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+
+class EventPublisher:
+    def __init__(self):
+        self.credentials = pika.PlainCredentials(settings.RABBITMQ_USER, settings.RABBITMQ_PASSWORD)
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=settings.RABBITMQ_HOST))
+        self.channel = self.connection.channel()
