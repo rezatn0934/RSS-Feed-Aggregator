64a1c30f9d86362bacce8dd6b8850d634e077b61
rezatn0934 <rezatn0934@gmail.com>
2023-10-11 14:28:05 +0330

    chore: add LoggingMiddleware to project MIDDLEWARE

--- config/settings.py
+++ config/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'accounts.middlewares.LoggingMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
