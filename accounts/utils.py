e99fb8e2a7e90e73d2ebd73f4186f93d2358e1bb
rezatn0934 <rezatn0934@gmail.com>
2023-10-11 14:24:33 +0330

    chore: define log_entry

--- accounts/utils.py
+++ accounts/utils.py
@@ -55,3 +55,35 @@ def custom_sen_mail(subject, message, receiver):
     recipient_list = [receiver, ]
     from_email = settings.EMAIL_HOST_USER
     send_mail(subject, message, from_email, recipient_list)
+
+
+def log_entry(request, response, exception=None):
+    remote_host = request.META.get('REMOTE_ADDR', ' ')
+    user_id = request.user.id if request.user.is_authenticated else ' '
+    user_info = {
+        'user_id': user_id,
+        'user_username': request.user.username if request.user.is_authenticated else ' ',
+        'user_email': request.user.email if request.user.is_authenticated else ' ',
+    }
+    request_line = f"{request.method} {request.get_full_path()} HTTP/1.1"
+    status_code = response.status_code if not exception else 500
+    response_size = response.get('Content-Length', ' ')
+    referer = request.META.get('HTTP_REFERER', ' ')
+    user_agent = request.META.get('HTTP_USER_AGENT', ' ')
+    elapsed_time = response.elapsed.total_seconds() if hasattr(response, 'elapsed') else None
+
+    message = str(exception) if exception else 'Request processed successfully'
+    event = f"{request.resolver_match.app_names}.{request.resolver_match.url_name}"
+    return {
+        'remote_host': remote_host,
+        'user_info': user_info,
+        'request_line': request_line,
+        'status_code': status_code,
+        'response_size': response_size,
+        'referer': referer,
+        'user_agent': user_agent,
+        'elapsed_time': elapsed_time,
+        'message': message,
+        'event': event,
+    }
+
