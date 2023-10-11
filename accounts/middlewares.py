308509b97cdd61b07c471cb9419c54b1b2bcd3b9
rezatn0934 <rezatn0934@gmail.com>
2023-10-11 14:10:12 +0330

    chore: define __call__ in LoggingMiddleware

--- accounts/middlewares.py
+++ accounts/middlewares.py
@@ -9,3 +9,9 @@ class LoggingMiddleware:
 
     def __init__(self, get_response):
         self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        log_data = log_entry(request, response)
+        log_to_elasticsearch(log_data, log_level='info')
+        return response
