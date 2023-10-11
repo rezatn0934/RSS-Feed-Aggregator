fb649d85604e566fddbe712be23d99de01690ba3
rezatn0934 <rezatn0934@gmail.com>
2023-10-11 14:11:36 +0330

    chore: define log_to_elasticsearch

--- accounts/middlewares.py
+++ accounts/middlewares.py
@@ -5,6 +5,13 @@ import logging
 logger = logging.getLogger('elastic-logger')
 
 
+def log_to_elasticsearch(log_data, log_level):
+    if log_level == 'info':
+        logger.info(json.dumps(log_data))
+    elif log_level == 'error':
+        logger.error(json.dumps(log_data))
+
+
 class LoggingMiddleware:
 
     def __init__(self, get_response):
