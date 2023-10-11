24a759740150feafd6ed7a1abc1a5bd8a2558173
rezatn0934 <rezatn0934@gmail.com>
2023-10-11 14:01:04 +0330

    chore: define emit in ElasticHandler

--- accounts/logger_handlers.py
+++ accounts/logger_handlers.py
@@ -12,3 +12,10 @@ class ElasticHandler(logging.Handler):
         super().__init__()
         self.es = Elasticsearch(f'http://{settings.ELASTICSEARCH_HOST}:{settings.ELASTICSEARCH_PORT}')
         self.sender = LogSender(self.es)
+
+    def emit(self, record):
+        try:
+            self.sender.writeLog(record, formatter=self.format)
+        except Exception:
+            self.handleError(record)
+
