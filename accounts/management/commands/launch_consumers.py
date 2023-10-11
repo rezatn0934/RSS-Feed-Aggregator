1b265357fa83fa0f828b276d13576622adee117b
rezatn0934 <rezatn0934@gmail.com>
2023-10-11 14:22:04 +0330

    chore: define handle in launch_consumers

--- accounts/management/commands/launch_consumers.py
+++ accounts/management/commands/launch_consumers.py
@@ -4,5 +4,17 @@ import threading
 
 
 class Command(BaseCommand):
-    help = 'Launches Consumer for login message : RabbitMQ'
+    help = 'Launches Consumer for login message: RabbitMQ'
 
+    def handle(self, *args, **options):
+        login_consumer = UserEventConsumer(event_type='login')
+        register_consumer = UserEventConsumer(event_type='register')
+        update_rss_consumer = UpdateRSSConsumer(event_type='update_rss')
+
+        thread1 = threading.Thread(target=login_consumer.consume_events, kwargs={'queue_name': 'login'})
+        thread2 = threading.Thread(target=register_consumer.consume_events, kwargs={'queue_name': 'register'})
+        thread3 = threading.Thread(target=update_rss_consumer.consume_events, kwargs={'queue_name': 'update_rss'})
+
+        thread1.start()
+        thread2.start()
+        thread3.start()
