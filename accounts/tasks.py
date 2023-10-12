970a6769905be80ffe619592734f666adf29a711
rezatn0934 <rezatn0934@gmail.com>
2023-10-12 12:06:16 +0330

    chore: define send_email_task celery task

---
+++ accounts/tasks.py
@@ -0,0 +1,14 @@
+from celery import shared_task
+
+from accounts.utils import custom_sen_mail
+from core.base_task import MyTask
+
+
+@shared_task(base=MyTask, bind=True, task_time_limit=60, acks_late=True)
+def send_email_task(self, reset_link, email):
+    custom_sen_mail('reset password', f"Your password rest link: {reset_link}", email)
+
+    return {
+        'status': 'success',
+        'message': f'Password reset email sent to {email}',
+    }
