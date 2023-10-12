483bf88e6b869fdcc6bef58a0079315642fe4080
rezatn0934 <rezatn0934@gmail.com>
2023-10-12 10:11:54 +0330

    chore: get rabbitmq configs from settings

--- accounts/consumer.py
+++ accounts/consumer.py
@@ -1,22 +1,20 @@
 import json
 import os
 import pika
+from django.conf import settings
 from abc import ABC, abstractmethod
 from interactions.models import Notification, Subscription
 from rssfeeds.models import Channel
 from .models import User
 
-RABBITMQ_HOST = os.environ.get("RABBITMQ_HOST")
-RABBITMQ_USER = os.environ.get("RABBITMQ_USER")
-RABBITMQ_PASSWORD = os.environ.get("RABBITMQ_PASSWORD")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 
 class EventConsumer(ABC):
     def __init__(self, event_type):
         self.event_type = event_type
-        self.credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASSWORD)
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
+        self.credentials = pika.PlainCredentials(settings.RABBITMQ_USER, settings.RABBITMQ_PASSWORD)
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=settings.RABBITMQ_HOST))
         self.channel = self.connection.channel()
 
     def declare_queue(self, queue_name):
