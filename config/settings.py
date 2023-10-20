58a9b2989537ad9e3dada3fe03a9a7aea66236e2
rezatn0934 <rezatn0934@gmail.com>
2023-10-20 09:55:29 +0330

    chore: define language configs

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_spectacular',
     'django_celery_beat',
+    'rosetta',
     'accounts',
     'core',
     'interactions',
@@ -54,6 +55,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -118,7 +120,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+LOCALE_PATHS = [BASE_DIR / "locale"]
 
+LANGUAGES = [
+    ('fa', _('Persian')),
+    ('en', _('English')),
+]
 TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
