4dd185c1b144d9a9f26a11d62f1b1b2006de331a
rezatn0934 <rezatn0934@gmail.com>
2023-10-22 23:39:59 +0330

    refactor: refactor PasswordTokenSerializer for data validations

--- accounts/serilizers.py
+++ accounts/serilizers.py
@@ -19,7 +19,10 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         del validated_data['password2']
-        return User.objects.create_user(**validated_data)
+        user = User.objects.create_user(**validated_data)
+        user.is_active = False
+        user.save()
+        return user
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
@@ -57,34 +60,51 @@ class ResetPasswordEmailSerializer(serializers.Serializer):
     email = serializers.EmailField(required=True)
 
 
+def validate_password_match(new_pass, new_pass2):
+    if new_pass != new_pass2:
+        raise serializers.ValidationError(_('Passwords must match!'))
+
+
+def validate_token_and_encoded_pk(token, encoded_pk):
+    if token is None or encoded_pk is None:
+        raise serializers.ValidationError(_("Missing data."))
+
+
+def validate_token(user, token):
+    if not PasswordResetTokenGenerator().check_token(user, token):
+        raise serializers.ValidationError(_("The reset token is invalid"))
+
+
+def publish_reset_password_event(user, request):
+    device_type = request.META.get('HTTP_USER_AGENT', 'UNKNOWN')
+    data = {
+        'user_id': user.id,
+        'data': f'{user.username} has reset his password with a URL link using {device_type}'
+    }
+    publish_event(event_type='reset_password', queue_name='reset_password', data=data)
+
+
 class PasswordTokenSerializer(serializers.Serializer):
     new_pass = serializers.CharField(style={"input_type": "password"}, write_only=True)
     new_pass2 = serializers.CharField(style={"input_type": "password"}, write_only=True)
 
     def validate(self, attrs):
-        if attrs['new_pass'] != attrs['new_pass2']:
-            raise serializers.ValidationError(_('Passwords must match!'))
-
-        password = attrs.get("new_pass")
+        new_pass = attrs.get("new_pass")
+        new_pass2 = attrs.get("new_pass2")
         token = self.context.get("kwargs").get("token")
         encoded_pk = self.context.get("kwargs").get("encoded_pk")
 
-        if token is None or encoded_pk is None:
-            raise serializers.ValidationError(_("Missing data."))
+        validate_password_match(new_pass, new_pass2)
 
         pk = urlsafe_base64_decode(encoded_pk).decode()
         user = User.objects.get(pk=pk)
-        if not PasswordResetTokenGenerator().check_token(user, token):
-            raise serializers.ValidationError(_("The reset token is invalid"))
 
-        user.set_password(password)
+        validate_token_and_encoded_pk(token, encoded_pk)
+        validate_token(user, token)
+
+        user.set_password(new_pass)
         user.save()
         request = self.context.get('request')
-        device_type = request.META.get('HTTP_USER_AGENT', 'UNKNOWN')
-        data = {
-            'user_id': user.id,
-            'data': f'{user.username} has reset his password with url link using {device_type}'}
-
-        publish_event(event_type='reset_password', queue_name='reset_password', data=data)
+        publish_reset_password_event(user, request)
 
         return attrs
