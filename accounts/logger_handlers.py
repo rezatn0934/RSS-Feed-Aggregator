9a9e941c30743b2a18412302352efc31d3767865
rezatn0934 <rezatn0934@gmail.com>
2023-10-22 23:35:18 +0330

    chore: add docstrings to logger_handlers.py

--- accounts/logger_handlers.py
+++ accounts/logger_handlers.py
@@ -7,6 +7,29 @@ from elasticsearch import Elasticsearch
 
 
 class ElasticHandler(logging.Handler):
+    """
+    Custom logging handler for sending log records to Elasticsearch.
+
+    This handler sends log records to Elasticsearch with a specified index name and timestamp.
+
+    Attributes:
+        es (Elasticsearch): Elasticsearch client instance for log storage.
+        sender (LogSender): Log sender instance for writing log records.
+
+    Methods:
+        emit(self, record):
+            Emit a log record to Elasticsearch.
+
+    Example Usage:
+        To use this custom logging handler, add it to your Django logging configuration.
+        It sends log records to Elasticsearch with the specified index name and timestamp.
+
+    Note:
+        - If there is an exception during log emission, it is handled, and the error is logged.
+        - The Elasticsearch host and port are configured from Django settings.
+
+    See the Elasticsearch Python client documentation for more details on usage.
+    """
 
     def __init__(self):
         super().__init__()
@@ -21,6 +44,33 @@ class ElasticHandler(logging.Handler):
 
 
 class LogSender:
+    """
+    Log sender for writing log records to Elasticsearch.
+
+    Attributes:
+        es (Elasticsearch): Elasticsearch client instance for log storage.
+
+    Methods:
+        writeLog(self, msg: logging.LogRecord, formatter):
+            Write a log record to Elasticsearch with a timestamp.
+
+    Args:
+        msg (logging.LogRecord): The log record to be written to Elasticsearch.
+        formatter (function): The log record formatter function.
+
+    Returns:
+        None
+
+    Example Usage:
+        This class is used by the `ElasticHandler` to format and send log records to Elasticsearch.
+
+    Note:
+        - Log records are sent to Elasticsearch with a timestamp and specified index name.
+        - The Elasticsearch client instance is provided during initialization.
+
+    See the Elasticsearch Python client documentation for more details on usage.
+    """
+
     def __init__(self, es):
         self.es = es
 
