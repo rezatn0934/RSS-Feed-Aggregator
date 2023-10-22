6b6d59dbab035c190246cfb685f0a34c6c63aad4
rezatn0934 <rezatn0934@gmail.com>
2023-10-22 23:35:57 +0330

    chore: define a flag to log once in cas of exceptions

--- accounts/middlewares.py
+++ accounts/middlewares.py
@@ -5,24 +5,22 @@ import logging
 logger = logging.getLogger('elastic-logger')
 
 
-def log_to_elasticsearch(log_data, log_level):
-    if log_level == 'info':
-        logger.info(json.dumps(log_data))
-    elif log_level == 'error':
-        logger.error(json.dumps(log_data))
-
-
 class LoggingMiddleware:
 
     def __init__(self, get_response):
         self.get_response = get_response
+        self.exception_occurred = False
 
     def __call__(self, request):
+        self.exception_occurred = False
+
         response = self.get_response(request)
         log_data = log_entry(request, response)
-        log_to_elasticsearch(log_data, log_level='info')
+        if not self.exception_occurred:
+            logger.info(json.dumps(log_data))
         return response
 
     def process_exception(self, request, exception):
+        self.exception_occurred = True
         log_data = log_entry(request, None, exception)
-        log_to_elasticsearch(log_data, log_level='error')
+        logger.error(json.dumps(log_data))
