01e4967a58f7b0a96834ecb2e7c7ef5de096becc
rezatn0934 <rezatn0934@gmail.com>
2023-10-23 12:17:06 +0330

    chore: define NewsDocumentView urls

--- rssfeeds/urls.py
+++ rssfeeds/urls.py
@@ -7,8 +7,9 @@ router.register('rssfeeds', views.XmlLinkViewSet)
 router.register('channels', views.ChannelViewSet)
 router.register('podcasts', views.PodcastViewSet)
 router.register('news', views.NewsViewSet)
-router.register('podcast', views.PodcastDocumentView, basename='podcast-document')
-router.register('channel', views.ChannelDocumentView, basename='channel-document')
+router.register('search_podcast', views.PodcastDocumentView, basename='search_podcast')
+router.register('search_channel', views.ChannelDocumentView, basename='search_channel')
+router.register('search_news', views.NewsDocumentView, basename='search_news')
 
 app_name = 'rssfeeds'
 urlpatterns = [
